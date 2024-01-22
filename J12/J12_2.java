94543b3e53c3dfa44af4ff3e6d46f2a691d60240
nina <ktq2390316@stu.o-hara.ac.jp>
2024-01-22 13:35:32 +0900

    Commit

--- J12/J12_2.java
+++ J12/J12_2.java
@@ -1,3 +1,28 @@
+class Person {
+    String name;
+    String job;
+
+    Person(String name, String job) { this.name = name; this.job = job; }
+
+    void construct() {
+    }
+
+    void introduce() {
+    }
+}
+
+class Teacher extends Person {
+    String subject;
+
+    Teacher(String name, String job, String subject) { super(name, job); this.subject = subject; }
+}
+
+class Cook extends Person {
+    String specialities;
+
+    Cook(String name, String job, String specialities) { super(name, job); this.specialities = specialities; }
+}
+
 public class J12_2 {
     
     public static void main(String[] args){
